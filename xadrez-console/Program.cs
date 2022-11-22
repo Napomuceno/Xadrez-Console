d736cd220f4e0e5efa388d73ec09b1a1ad86a7d8
Jefferson Napomuceno <erjeffersonnapomuceno@gmail.com>
2022-11-21 23:16:45 -0300

    Melhorando a implementação do metodo imprimirtabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,13 +8,26 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Console.WriteLine(pos.ToPosicao());
 
-            Console.WriteLine();
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.ImprimirTabuleiro(tab);
+
+                Console.WriteLine();
+            }
+
+            catch(TabueleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -10,8 +10,10 @@ namespace xadrez_console
         {
             for(int i =0; i<tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j<tab.Colunas; j++)
                 {
+                    
                     if (tab.peca(i,j) == null)
                     {
                         Console.Write("- ");
@@ -19,13 +21,34 @@ namespace xadrez_console
 
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        ImprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
 
                     
-                }
 
+                }
+                
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+
+                Console.ForegroundColor = aux;
             }
         }
     }
